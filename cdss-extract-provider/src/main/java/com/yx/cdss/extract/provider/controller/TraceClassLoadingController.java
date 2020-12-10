350322ad4fdd2c92f14346f20210bdbca88365c1
John-Goo <guhaijiang666@hotmail.com>
2020-12-10 21:53:37 +0800

    增加安全访问控制

--- cdss-extract-provider/src/main/java/com/yx/cdss/extract/provider/controller/TraceClassLoadingController.java
+++ cdss-extract-provider/src/main/java/com/yx/cdss/extract/provider/controller/TraceClassLoadingController.java
@@ -2,9 +2,11 @@ package com.yx.cdss.extract.provider.controller;
 
 import com.yx.cdss.extract.provider.bo.CalcRequestBo;
 import com.yx.cdss.extract.provider.bo.JStu;
+import com.yx.cdss.extract.provider.bo.StudentRequBo;
 import com.yx.cdss.extract.provider.common.WResult;
 import com.yx.cdss.extract.provider.filter.WAuth;
 import com.yx.cdss.extract.provider.filter.WAuthEnum;
+import com.yx.cdss.extract.provider.util.WSyncDataHelper;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +18,7 @@ public class TraceClassLoadingController {
 
 
 
-    @WAuth(value = WAuthEnum.ADMIN)
+   // @WAuth(value = WAuthEnum.ADMIN)
     @ApiOperation(value = "演示class 加载过程" )
     @RequestMapping(value = "/my/classloading", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
     public WResult classLoad(@RequestBody CalcRequestBo requestBo) {
@@ -28,6 +30,20 @@ public class TraceClassLoadingController {
             stuLsit.add(jStu);
         }
         wResult.ok(stuLsit);
+        /**
+         * 同步数据
+         */
+        String url = "http://localhost:8082/queryInfo";
+        StudentRequBo requBo = new StudentRequBo();
+        requBo.setName("John");
+        requBo.setIdCard("66666666661");
+        requBo.setAddr("深圳");
+        WResult<StudentRequBo> wResult1 = WSyncDataHelper.builder()
+                .setEntity(requBo)
+                .post(url)
+                .execute(StudentRequBo.class);
+        System.out.println(" >>>"+wResult1.getData());
+
         return wResult;
     }
 
